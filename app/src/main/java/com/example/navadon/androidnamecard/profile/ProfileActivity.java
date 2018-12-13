128a3c1603d36bf90db6f9a495e43dfa3eab8b0c
molysulfur <molysulfur.mos@gmail.com>
2018-12-14 02:21:28 +0700

    binding data to the User model and cannot click to save into firebase

--- app/src/main/java/com/example/navadon/androidnamecard/profile/ProfileActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/profile/ProfileActivity.java
@@ -4,11 +4,13 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.navadon.androidnamecard.R;
 import com.example.navadon.androidnamecard.databinding.ActivityProfileBinding;
+import com.example.navadon.androidnamecard.model.User;
 
 public class ProfileActivity extends AppCompatActivity {
     private ProfileViewModel profileViewModel;
@@ -16,30 +18,29 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initInstances();
+    }
+
+    private void initInstances() {
         profileViewModel = new ProfileViewModel();
         profileBinding = DataBindingUtil.setContentView(this, R.layout.activity_profile);
         profileBinding.setViewModel(profileViewModel);
+        String userId = getIntent().getStringExtra("userId");
 
-        setListener();
+        profileViewModel.getInformationWithFirebase(userId);
+        setImageUrl();
     }
 
-    private void setListener() {
-        // set text watcher
-        profileBinding.etUsername.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    private void setImageUrl() {
+        try{
+            Glide.with(this)
+                    .load(profileViewModel.users.get().imageUrl)
+                    .into((ImageView) findViewById(R.id.img_profile));
+        }catch (NullPointerException e){
+            Log.e("Set Image Profile", "You don't set profile in google");
+        }
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                profileViewModel.username.set(s.toString());
-            }
-        });
     }
+
+
 }
--- app/src/main/java/com/example/navadon/androidnamecard/profile/ProfileViewModel.java
+++ app/src/main/java/com/example/navadon/androidnamecard/profile/ProfileViewModel.java
@@ -2,14 +2,49 @@ package com.example.navadon.androidnamecard.profile;
 
 import android.arch.lifecycle.ViewModel;
 import android.databinding.ObservableField;
+import android.support.annotation.NonNull;
 import android.util.Log;
+
+import com.example.navadon.androidnamecard.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class ProfileViewModel extends ViewModel {
 
-    public ObservableField<String> username = new ObservableField<>("test");
+    private DatabaseReference mFirebaseDatabase;
+    public ObservableField<User> users = new ObservableField<>();
+
+    private ValueEventListener valueFromGoogle = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            Map map = (Map) dataSnapshot.getValue();
+            User user = new User();
+            user.firstname = (String) map.get("firstname");
+            user.lastname = (String) map.get("lastname");
+            user.email = (String) map.get("email");
+            user.imageUrl = (String) map.get("imageUrl");
+            users.set(user);
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
 
     public void save(){
-        Log.e("Profile ViewModel", username.get());
+
+        Log.e("Profile ViewModel", users.get().getFirstname());
+    }
+
+    public void getInformationWithFirebase(String userId){
+        mFirebaseDatabase = FirebaseDatabase.getInstance().getReference();
+        mFirebaseDatabase.child("users").child(userId).addValueEventListener(valueFromGoogle);
     }
 
 }
