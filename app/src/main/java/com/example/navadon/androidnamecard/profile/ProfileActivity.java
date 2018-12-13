7c1dc36ac4bd044542ce63380b650b46af9071c2
bombubble <bombubble@gmail.com>
2018-12-13 18:50:31 +0700

    popup menu

--- app/src/main/java/com/example/navadon/androidnamecard/profile/ProfileActivity.java
+++ app/src/main/java/com/example/navadon/androidnamecard/profile/ProfileActivity.java
@@ -6,6 +6,11 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.example.navadon.androidnamecard.R;
 import com.example.navadon.androidnamecard.databinding.ActivityProfileBinding;
@@ -13,6 +18,7 @@ import com.example.navadon.androidnamecard.databinding.ActivityProfileBinding;
 public class ProfileActivity extends AppCompatActivity {
     private ProfileViewModel profileViewModel;
     ActivityProfileBinding profileBinding;
+    ImageView icon;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +27,22 @@ public class ProfileActivity extends AppCompatActivity {
         profileBinding.setViewModel(profileViewModel);
 
         setListener();
+
+        icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(ProfileActivity.this, icon);
+                popupMenu.getMenuInflater().inflate(R.menu.menu_main, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(ProfileActivity.this, ""+ item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+            }
+        });
     }
 
     private void setListener() {
